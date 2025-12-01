f29d03d202b375e5737d4ceee25b517be2be8edf
mangert <mangert@yandex.ru>
2025-12-02 00:52:26 +0300

    bitboard

--- src/Bits.cpp
+++ src/Bits.cpp
@@ -49,8 +49,7 @@ public:
 	//считаем отнимая единички
 	uint64_t static count_bits_sub_one(uint64_t number) {
 		uint64_t count = 0;
-		while (number != 0) {
-			
+		while (number != 0) {			
 			number &= (number - ONE);
 			++count;
 		}
@@ -70,51 +69,114 @@ public:
 	};	
 
 	//шахматные задачки
-	
-	moves knigth(uint64_t initial_pos) {
-		moves result; //переменная для результата
-		
-		//начальная позиция
-		uint64_t bits_pos = BIT(initial_pos);		
+	//про коня
+	moves static knight(uint64_t initial_pos) {
+		if (initial_pos > 63)
+			throw std::out_of_range("Position must be between 0 and 63");
+		moves result;
+		uint64_t knight_bit = BIT(initial_pos);
 
-		//исключаемые зоны
-		uint64_t to_right_1 = 0xFeFeFeFeFeFeFeFe; //при движении вправо на 1 клетку за доску
-		uint64_t to_right_2 = 0xFcFcFcFcFcFcFcFc; //при движении вправо на 2 клетки за доску
-		uint64_t  to_left_1 = 0x7f7f7f7f7f7f7f7f; //при движении влево на 1 клетку за доску
-		uint64_t to_left_2 = 0x3f3f3f3f3f3f3f3f; //при движении влево на 2 клетки за доску
+		// Маски для исключения выходов за границы доски
+		uint64_t no_right_1 = 0xFeFeFeFeFeFeFeFe;  // Без правого края
+		uint64_t no_right_2 = 0xFcFcFcFcFcFcFcFc;  // Без 2 правых краев
+		uint64_t no_left_1 = 0x7f7f7f7f7f7f7f7f;  // Без левого края  
+		uint64_t no_left_2 = 0x3f3f3f3f3f3f3f3f;  // Без 2 левых краев
 
-		result.mask = to_left_2 & (bits_pos << 6 | bits_pos >> 10)
-			| to_left_1 & (bits_pos << 15 | bits_pos >> 17)
-			| to_right_1 & (bits_pos << 17 | bits_pos >> 15)
-			| to_right_2 & (bits_pos << 10 | bits_pos >> 6);
+		result.mask = no_left_2 & (knight_bit << 6 | knight_bit >> 10)
+			| no_left_1 & (knight_bit << 15 | knight_bit >> 17)
+			| no_right_1 & (knight_bit << 17 | knight_bit >> 15)
+			| no_right_2 & (knight_bit << 10 | knight_bit >> 6);
 
-
+		result.count = count_bits_cashed(result.mask);
+		return result;
 	};
 
-	/*
-	void king(std::string s_pos) {
-		int point;
-		s = s.ToUpper();
-		try {
-			point = int.Parse(s);
-		}
-		catch {
-			try {
-				point = (s[0] - 'A') + (s[1] - '1') * 8;
-			}
-			catch (Exception ex) {
-				Console.SetCursorPosition(0, 10);
-				Console.WriteLine(ex.Message);
-				return;
-			}
-		}
-		bb = (ulong)1 << point;
-	}*/
-//служебные функции
-private:
+	//про короля
+	moves static king(uint64_t initial_pos) {
+		if (initial_pos > 63)
+			throw std::out_of_range("Position must be between 0 and 63");
+		
+		moves result;		
+		uint64_t king_bit = BIT(initial_pos);
 
+		// Маски для исключения выходов за границы доски
+		uint64_t no_right_edge = 0xfefefefefefefefe;  // Без правого края		
+		uint64_t no_left_edge = 0x7f7f7f7f7f7f7f7f;  // Без левого края  
+		
+		result.mask = (no_left_edge & (king_bit << 7 | king_bit >> 1 | king_bit >> 9)) |
+			(king_bit << 8 | king_bit >> 8) |
+			(no_right_edge & (king_bit << 9 | king_bit << 1 | king_bit >> 7));
+		
+		result.count = count_bits_cashed(result.mask);
+		return result;
+	};
 
+	//про ладью
+	moves static rook(uint64_t initial_pos) {
+		if (initial_pos > 63)
+			throw std::out_of_range("Position must be between 0 and 63");
 
+		moves result;
+		uint64_t rook_bit = BIT(initial_pos);
+
+		int row = initial_pos / 8;    // Номер строки (0-7)
+		int col = initial_pos % 8;    // Номер столбца (0-7)
+
+		// Вертикаль: все биты столбца col
+		uint64_t vertical = 0x0101010101010101ULL << col;
+		// Горизонталь: все биты строки row  
+		uint64_t horizontal = 0xFFULL << (row * 8);
+
+		result.mask = (vertical | horizontal) ^ rook_bit;
+		result.count = count_bits_cashed(result.mask);
+		return result;
+	};
+
+	//про слона
+	moves static bishop(uint64_t initial_pos) {
+		if (initial_pos > 63) 
+			throw std::out_of_range("Position must be between 0 and 63");
+		
+		moves result;
+		uint64_t bishop_bit = BIT(initial_pos);
+
+		int row = initial_pos / 8;    // Номер строки (0-7)
+		int col = initial_pos % 8;    // Номер столбца (0-7)
+
+		// Главная диагональ
+		uint64_t main_diag = 0x8040201008040201;
+		//сдвигаем главную диагональ на разницу, масштабированную на высоту доски
+		main_diag = (row < col) ?
+			main_diag >> ((col - row) * 8) :
+			main_diag << ((row - col) * 8);	
+		
+		// Побочная диагональ
+		uint64_t anti_diag = 0x102040810204080;
+		//сдвигаем главную диагональ на разницу, масштабированную на высоту доски
+		int rev_col = 7 - col; //разницу берем от конца
+		anti_diag = (row < rev_col) ?
+			anti_diag >> ((rev_col - row) * 8):
+			anti_diag << ((row - rev_col) * 8);		
+		
+		result.mask = (main_diag | anti_diag) ^ bishop_bit;		
+		result.count = count_bits_cashed(result.mask);
+		return result;
+	};
+
+	//про ферзя
+	moves static queen(uint64_t initial_pos) {		
+		
+		if (initial_pos > 63)
+			throw std::out_of_range("Position must be between 0 and 63");
+		
+		uint64_t rook_mask = rook(initial_pos).mask;
+		uint64_t bishop_mask = bishop(initial_pos).mask;
+		
+		moves result;
+		result.mask = rook_mask | bishop_mask;
+		result.count = count_bits_cashed(result.mask);
+		return result;
+	};
 
 
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,31 @@
 
 int main() {
 
-	
-	
+	//1000000001000000001000000001000000001000000001000000001000000001
+	//0001000000001000000001000000001000000001000000000000000000000000
+	//9241421688590303745
+	//1155177711073755136
+		
+	//Bits::print_bitboard(1155177711073755136);
+	//uint64_t a = 9241421688590303745;
+	//std::cout << a << std::endl;
+	//std::cout << (a / 8) << std::endl;
+
+	/*
 	uint64_t num = 1024ULL;
 	Bits::print_bitboard(num);
 
 	std::cout << Bits::count_bits_naiv(num) << std::endl;
 	std::cout << Bits::count_bits_sub_one(num) << std::endl;
 	std::cout << Bits::count_bits_cashed(num) << std::endl;
+	std::cout << "_______________" << std::endl;
 
+	Bits::moves r = Bits::rook(34);
+	Bits::print_bitboard(r.mask);
+	std::cout << "_______________" << std::endl;
+	*/
+	Bits::moves b = Bits::king(32);
+	Bits::print_bitboard(b.mask);
 	
 	
 	return 0;
